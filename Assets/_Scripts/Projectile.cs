9ec07a0dc9f1044d857dfce6ff04623ba827559d
ESmittyLTU <esmith8@ltu.edu>
2024-11-14 14:16:21 -0500

    Projectile motion
    added projectile motion methods and variables necessary for cross-class comms

--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-// Your mom
 
-    // Update is called once per frame
-    void Update()
+    // Controls projectile speed
+    public float projectileSpeed = 1f;
+
+    // target game object for when player is aiming at a specific enemy
+    private GameObject trackingTarget;
+
+
+    // This is called f
+    void Update() 
     {
-        
+        if (trackingTarget != null)
+        {
+            trackingProj();
+        }
+
     }
+
+    //Method called after projectile instantiated in player class,
+    //determines if whether to fire projectile straight or track onto enemy 
+    public void startProj(GameObject receiver)
+    {
+        if (receiver == null)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * projectileSpeed;
+        } else
+        {
+            trackingTarget = receiver; 
+        }
+    }
+
+    //Tells projectile to track to its target and move to that pos
+    public void trackingProj()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, trackingTarget.transform.position, projectileSpeed * Time.deltaTime);
+    }
+
+
 }
