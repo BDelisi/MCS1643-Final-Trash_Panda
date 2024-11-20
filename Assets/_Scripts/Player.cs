71cd516a153fde0ec5a7c16ef58fe968ce8313a2
BDelisi <bdelisi@ltu.edu>
2024-11-20 12:20:23 -0500

    Added Enemy Attacking
    Made a system for rats to actually be able to damage the player

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -6,17 +6,29 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int health = 10;
+    private float iFrames = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (iFrames != 0)
+        {
+            iFrames -= Time.deltaTime;
+            if (iFrames < 0)
+            {
+                iFrames = 0;
+            }
+        }
     }
 
     // Method to be called by projectile that makes the player take damage according to the spell
     public void loseHealth(int projDamage)
     {
-        health -= projDamage;
+        if (iFrames <= 0)
+        {
+            health -= projDamage;
+            iFrames = .25f;
+        }
     }
 
 }
--- Assets/_Scripts/RatBehavior.cs
+++ Assets/_Scripts/RatBehavior.cs
@@ -1,16 +1,18 @@
 using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class RatBehavior : MonoBehaviour
 {
     public GameObject target;
+    public int damage = 1;
     private NavMeshAgent agent;
-    private bool attacking;
+    private float movingCd;
     private GameObject player;
-    
+     
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -23,6 +25,28 @@ public class RatBehavior : MonoBehaviour
     void FixedUpdate()
     {
         //tells the NavMesh to pathfind towards the target
-        agent.destination = target.transform.position;
+        if (movingCd == 0)
+        {
+            agent.destination = target.transform.position;
+        }
+        else
+        {
+            movingCd -= Time.deltaTime;
+            agent.destination = transform.position;
+            if (movingCd < 0)
+            {
+                movingCd = 0;
+            }
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && movingCd <= 0)
+        {
+            collision.gameObject.GetComponent<Player>().loseHealth(damage);
+            movingCd = .5f;
+        }
     }
 }
