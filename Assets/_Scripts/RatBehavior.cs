cecdafa5e8562cbce306246f8c0d5b3a427724f5
BDelisi <bdelisi@ltu.edu>
2024-12-08 23:23:44 -0500

    Added the nav mesh to the sewer scene
    Also figured out the doors so that the rats can pathfind through the doors

--- Assets/_Scripts/RatBehavior.cs
+++ Assets/_Scripts/RatBehavior.cs
@@ -12,22 +12,32 @@ public class RatBehavior : MonoBehaviour
     private NavMeshAgent agent;
     private float movingCd;
     private GameObject player;
-     
+    private Animator animations;
+    private Vector3 lastPos;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = FindObjectOfType<FirstPersonController>().gameObject;
         target = player;
         agent.destination = target.transform.position;
+        animations = GetComponent<Animator>();
+        lastPos = transform.position;
     }
 
-   
+    private void Update()
+    {
+        animations.SetFloat("currentSpeed", Vector3.Distance(transform.position, lastPos) * Time.deltaTime);
+        lastPos = transform.position;
+    }
+
     void FixedUpdate()
     {
         //tells the NavMesh to pathfind towards the target
         if (movingCd == 0)
         {
             agent.destination = target.transform.position;
+            
         }
         else
         {
