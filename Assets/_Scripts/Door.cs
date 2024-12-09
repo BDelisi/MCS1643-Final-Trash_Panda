ba061831679f55dd8452c7b757bd4ddeb1321fe7
ESmittyLTU <esmith8@ltu.edu>
2024-12-08 20:08:34 -0500

    level dev complete (little juice)

--- Assets/_Scripts/Door.cs
+++ Assets/_Scripts/Door.cs
@@ -1,36 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     public bool closeOnExit = false;
-    public int openSpeed = 1;
+    public float openSpeed = 1f;
+    public float closeDelay = 1f;
+    bool isOpen = false;
+    bool doorClosing = false;
+    bool opening = false;
+
+    GameObject grate;
 
     //Declares origPos V3 (so it can store origPos)
     Vector3 origPos;
+    Vector3 targPos;
 
     void Start()
     {
-        origPos = transform.position;
+        grate = transform.GetChild(0).gameObject;
+        origPos = grate.transform.position;
+        targPos = new Vector3(grate.transform.position.x, grate.transform.position.y + 2, grate.transform.position.z);
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        //Door closing controls the delay between invoke and movetowards so if the player reenters the collider during that delay, the door wont close
+        doorClosing = false;
+        Debug.Log("Trigger works");
+        if (other.CompareTag("Player") && isOpen == false)
         {
-            transform.position = Vector3.MoveTowards(origPos, new Vector3(transform.position.x, transform.position.y + 3, transform.position.z), openSpeed * Time.deltaTime);
+            Debug.Log("Door up");
+            while (targPos.y > grate.transform.position.y)
+            {
+                grate.transform.position = Vector3.MoveTowards(grate.transform.position, targPos, openSpeed * Time.deltaTime);
+            }
+            isOpen = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player") && closeOnExit)
+        if (other.CompareTag("Player") && closeOnExit && isOpen)
         {
-            transform.position = Vector3.MoveTowards(transform.position, origPos, openSpeed * Time.deltaTime);
+            doorClosing = true;
+            Invoke("closeAfterDelay", closeDelay);
+            isOpen = false;
         }
     }
 
-    
+    void closeAfterDelay()
+    {
+        if (doorClosing)
+        {
+            while (origPos.y < grate.transform.position.y)
+            {
+                grate.transform.position = Vector3.MoveTowards(grate.transform.position, origPos, openSpeed * Time.deltaTime);
+            }
+        }
+    }
+
 }
